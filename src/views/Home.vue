3488c9b945d41679f6c2f4183d52a645e3b3925b
Kamonpob <kamonpob@TH-LW11327-m.local>
2018-10-01 22:23:59 +0700

    fix code

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -6,9 +6,12 @@
       </div>
       <div class="hero-body">
         <div class="container">
-          <div style="padding-bottom: 10px;" v-for="(index, item) in 5" :key="index" >
-            <h1 class="title">Large title {{ item }}</h1>
-            <h2 class="subtitle">Large subtitle</h2>
+          <div class="has-text-right">
+            <span>{{ data }}</span>
+          </div>
+          <div style="padding-bottom: 10px;" :class="{'border-select': select === item.name}" v-for="(item, index) in list" :key="index" >
+            <h1 class="title" @click="selectBox(item.name)">{{ item.name }}</h1>
+            <h2 class="subtitle"> {{ item.birth_year }} </h2>
           </div>
         </div>
       </div>
@@ -18,18 +21,41 @@
 </template>
 
 <script>
+import axios from 'axios'
 const liff = window.liff
 export default {
   name: 'home',
   data () {
     return {
-      data: null
+      data: null,
+      list: [],
+      select: ''
     }
   },
-  mounted () {
+  async mounted () {
+    for (let i = 1; i <= 5; i++) {
+      let { data } = await axios.get(`https://swapi.co/api/people/${i}/`)
+      this.list.push(data)
+    }
+    if (localStorage.getItem('storageSelect')) {
+      this.select = localStorage.getItem('storageSelect')
+    }
     liff.init((data) => {
       this.data = data
     })
+  },
+  methods: {
+    selectBox (name) {
+      this.select = name
+      localStorage.setItem('storageSelect', name)
+    }
   }
 }
 </script>
+
+<style>
+.border-select {
+  border: 2px solid green;
+}
+
+</style>
