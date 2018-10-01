78f96e852925da9cfc48c68031924befa0f6e27b
Kamonpob <kamonpob@TH-LW11327-m.local>
2018-10-01 21:44:38 +0700

    create form

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,18 @@
 <template>
   <div>
-    Hello world
+    <section class="hero is-fullheight">
+      <div class="hero-head is-info">
+        <h1> LINE APIs </h1>
+      </div>
+      <div class="hero-body">
+        <div class="container">
+          <div style="padding-bottom: 10px;" v-for="(index, item) in 5" :key="index" >
+            <h1 class="title">Large title {{ item }}</h1>
+            <h2 class="subtitle">Large subtitle</h2>
+          </div>
+        </div>
+      </div>
+    </section>
     {{ data }}
   </div>
 </template>
